7027992ac432536f2751d36e771af96e2b5c0130
Bourbon0212 <dx7880880212@gmail.com>
2018-08-02 19:50:51 +0800

    4

--- Final Project/Youbike_Map.R
+++ Final Project/Youbike_Map.R
@@ -1,11 +1,14 @@
 library(ggmap)
 library(ggplot2)
+library(gganimate)
 library(viridis)
 library(RColorBrewer)
 library(raster)
+library(tidyr)
+library(magick)
 
-res <- read.csv('D:/GitHub/NTU-CS-X/Final Project/Youbike_res.csv')
-sbi <- read.csv('D:/GitHub/NTU-CS-X/Final Project/Youbike_sbi.csv')
+res <- read.csv('D:/GitHub/NTU-CS-X/Final Project/Youbike/data/Youbike_res.csv')
+sbi <- read.csv('D:/GitHub/NTU-CS-X/Final Project/Youbike/data/Youbike_sbi.csv')
 
 # Map
 map <- get_map(location = c(min(res$lng), min(res$lat), max(res$lng), max(res$lat)), maptype = "toner-lite")
@@ -19,3 +22,14 @@ res.stat.map <- ggmap(map) %+% res + aes(x = lng, y = lat, z = X307) +
   ggtitle('Remaining Amount of Youbike in Taipei')
 
 print(res.stat.map)
+
+res_g <- gather(res, time, per, X307:X318)
+
+# Animation
+res.stat.map.ani <- ggmap(map) %+% res + aes(x = lng, y = lat, z = X307, frame = time) +
+  stat_summary_2d(fun = median, alpha = 0.6) +
+  scale_fill_gradientn(name = 'Median', colours = brewer.pal(11, "RdYlGn"), space = 'Lab') +
+  labs(x = "Longitude", y = "Latitude") +
+  coord_map()
+
+gg_animate(res.stat.map.ani, interval = 1.0, filename = 'vocab.gif')
